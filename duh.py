6b6225745ee51da83b8352ba8aa327015736a3dc
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-12-23 10:37:20 -0600

    Pretty print exactly 2 symbols.

--- duh.py
+++ duh.py
@@ -4,11 +4,44 @@ import wave
 filestr = sys.argv[1] # fixme catch exception, "with"
 wavfile = wave.open(filestr, 'r')
 
+baud = 50
+frate = wavfile.getframerate()
+frames_per_symbol = frate / baud
+
 print("params", wavfile.getparams())
+print()
+
 five = wavfile.readframes(5)
 fivemore = wavfile.readframes(5)
 print(five)
 print(five.hex())
 print(fivemore.hex())
-print(type(five))
-print ("seconds", wavfile.getnframes() / wavfile.getframerate())
+print()
+
+print ("seconds", wavfile.getnframes() / frate)
+print("frames / symb =", frames_per_symbol)
+wavfile.rewind()
+two_symbols = wavfile.readframes(int(frames_per_symbol * 2))
+print("bytes in 2 symb", len(two_symbols))
+print()
+
+hstring = two_symbols.hex()
+
+def pretty_hstring(hs):
+    bytes_space = 2
+    bytes_newline = 16
+    cs = bytes_space * 2 # chars per space
+    cn = bytes_newline * 2 # chars per newline
+    for n, c in enumerate(hs):
+        # every 16 bytes add a newline
+        # fixme insert code here
+        if n % cn == cn - 1:
+            yield (c + "\n")
+        elif n % cs == cs - 1:
+            # Every 4 char (2 bytes), add a space.
+            yield (c + " ") # fixme should we yield 2 things?
+        else:
+            yield c
+
+plist = list(pretty_hstring(hstring))
+print(''.join(plist))
