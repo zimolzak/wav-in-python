29ec523560042c729c9cc80695a2bd259cd77b7c
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-12-23 15:51:20 -0600

    More efficient and structured compute. Docstrings too.

--- duh.py
+++ duh.py
@@ -4,32 +4,33 @@ from wave_helpers import pretty_hex_string, bytes2int_list, ints2dots
 
 wav_file = wave.open(sys.argv[1], 'r')  # fixme catch exception, "with"
 
-baud = 50
+baud = 50  # symbol / sec
 frame_rate = wav_file.getframerate()
 frames_per_symbol = frame_rate / baud
+n_symbols = 2
+symbols = wav_file.readframes(int(frames_per_symbol * n_symbols))
 
-print("params:\n", wav_file.getparams())
-print()
-
-five = wav_file.readframes(5)
-print("1st 5 frames raw:", five)
-print("seconds =", wav_file.getnframes() / frame_rate)
-print("frames / symbol =", frames_per_symbol)
-
-wav_file.rewind()
-two_symbols = wav_file.readframes(int(frames_per_symbol * 2))
-print("bytes in 2 symbols =", len(two_symbols))
-print()
-
-# Make several objects for printing
-
-pretty_hex_list = list(pretty_hex_string(two_symbols.hex()))
-int_list = list(bytes2int_list(two_symbols))
+# Make 3 objects for printing
+pretty_hex_list = list(pretty_hex_string(symbols.hex()))
+int_list = list(bytes2int_list(symbols))
 dot_list = list(ints2dots(int_list))
 
+# Do the printing
+
 n_bytes = 16
-print("first %i bytes:\n" % n_bytes)
-print(''.join(pretty_hex_list[:n_bytes * 2]))
-print(int_list[:n_bytes // 2])
+byte_per_sample = wav_file.getsampwidth()
+n_frames = n_bytes / byte_per_sample
+char_per_byte = 2  # A byte is two hex digits '01' or '0F' etc.
+
+print("Params:\n", wav_file.getparams())
 print()
-print('\n'.join(dot_list[:n_bytes // 2]))
+print("Seconds =", wav_file.getnframes() / frame_rate)
+print("Frames / FSK symbol =", frames_per_symbol)
+print("Bytes in %i FSK symbols =" % n_symbols, len(symbols))
+print()
+print("First %i bytes (%i frames):" % (n_bytes, n_frames))
+print(symbols[:n_bytes])
+print(''.join(pretty_hex_list[:n_bytes * char_per_byte]))
+print(int_list[:n_bytes // byte_per_sample])
+print()
+print('\n'.join(dot_list[:n_bytes // byte_per_sample]))
--- wave_helpers.py
+++ wave_helpers.py
@@ -1,5 +1,5 @@
 def pretty_hex_string(hs):  # fixme params for 2 and 16 magic nums
-    """Input a string and add spaces and newlines every so often."""
+    """Input a string. Yield a stream of chars with spaces and newlines added every so often."""
     bytes_space = 2  # fixme make a function arg
     bytes_newline = 16
     cs = bytes_space * 2  # chars per space
@@ -17,15 +17,17 @@ def pretty_hex_string(hs):  # fixme params for 2 and 16 magic nums
 
 
 def bytes2int_list(byte_list):
+    """Input a 'bytes' object. Add pairs of bytes together & yield generator of ints."""
     for n, b in enumerate(byte_list):
         if n % 2 == 0:
             continue
         else:
-            yield 256 * byte_list[n - 1] + byte_list[n]
+            yield 256 * byte_list[n - 1] + byte_list[n]  # fixme endian?
             # indexing or list() on a 'bytes' obj auto-converts to 'int'
 
 
 def ints2dots(ints):
+    """Input a list of ints. Yield strings that make a bar graph of the ints."""
     max_int = 256 * 255 + 255  # 65535
     max_spaces = 75
     for x in ints:
