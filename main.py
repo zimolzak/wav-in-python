8d237de623aef60c9052a53e3eadb0c4c315d8c7
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-07 10:25:26 -0600

    Consistent terms (frame/sample) and more logical order.

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ from wave_helpers import pretty_hex_string, bytes2int_list, ints2dots
 
 wav_file = wave.open(sys.argv[1], 'r')  # fixme catch exception, "with"
 
-# new findings by inspecting my particular FSK WAV file
-# start about sample 3080, then take the 2 symbols.
-# Looks like a good mark & space pair
-# for purpose of bar graph it only takes 15 samples
-
-first_sample = 3080
+# Free parameters
+first_sample = 3080  # start of good mark/space in sample-data.wav
 baud = 50  # symbol / sec
-frame_rate = wav_file.getframerate()
-frames_per_symbol = frame_rate / baud
-n_symbols = 2
+n_symbols_to_read = 2
 
+# Calculated and derived vars
+sample_rate = wav_file.getframerate()
+bytes_per_sample = wav_file.getsampwidth()
+samples_per_symbol = sample_rate / baud
+n_samples_to_read = int(samples_per_symbol * n_symbols_to_read)
+
+# Read from file
 wav_file.setpos(first_sample)
-symbols = wav_file.readframes(int(frames_per_symbol * n_symbols))
+symbols = wav_file.readframes(n_samples_to_read)
 
 # Make 3 objects for printing
 pretty_hex_list = list(pretty_hex_string(symbols.hex()))
@@ -24,22 +25,21 @@ int_list = list(bytes2int_list(symbols))
 dot_list = list(ints2dots(int_list))
 
 # Do the printing
-
-
-n_frames = 15
-byte_per_sample = wav_file.getsampwidth()
-n_bytes = n_frames * byte_per_sample
+n_frames_to_plot = 15
+n_bytes_to_plot = n_frames_to_plot * bytes_per_sample
 char_per_byte = 2  # A byte is two hex digits '01' or '0F' etc.
 
 print("Params:\n", wav_file.getparams())
 print()
-print("Seconds =", wav_file.getnframes() / frame_rate)
-print("Frames / FSK symbol =", frames_per_symbol)
-print("Bytes in %i FSK symbols =" % n_symbols, len(symbols))
+print("File duration (s) =", wav_file.getnframes() / sample_rate)
+print("Frames / FSK symbol =", samples_per_symbol)
+print("Bytes in %i FSK symbols =" % n_symbols_to_read, len(symbols))
 print()
-print("First %i bytes (%i frames):" % (n_bytes, n_frames))
-print(symbols[:n_bytes])
-print(''.join(pretty_hex_list[:n_bytes * char_per_byte]))
-print(int_list[:n_bytes // byte_per_sample])
+print("First %i bytes (%i samples):" % (n_bytes_to_plot, n_frames_to_plot))
+print(symbols[:n_bytes_to_plot])
 print()
-print('\n'.join(dot_list[:n_bytes // byte_per_sample]))
+print(''.join(pretty_hex_list[:n_bytes_to_plot * char_per_byte]))
+print()
+print(int_list[:n_bytes_to_plot // bytes_per_sample])
+print()
+print('\n'.join(dot_list[:n_bytes_to_plot // bytes_per_sample]))
