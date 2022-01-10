99a72b07133fb101cee1c9aa951bc1d289f9c571
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-01-10 15:27:58 -0600

    Rudimentary short time Fourier transform w/ scipy.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import wave
 from wave_helpers import pretty_hex_string, bytes2int_list, ints2dots
+from scipy import signal
 
 wav_file = wave.open(sys.argv[1], 'r')  # fixme catch exception, "with"
 
 # Free parameters
-first_sample = 3080  # start of good mark/space in sample-data.wav
+start_sample = 3080  # start of good mark/space in sample-data.wav
 baud = 50  # symbol / sec
 n_symbols_to_read = 2
 
@@ -16,12 +17,12 @@ samples_per_symbol = sample_rate / baud
 n_samples_to_read = int(samples_per_symbol * n_symbols_to_read)
 
 # Read from file
-wav_file.setpos(first_sample)
-symbols = wav_file.readframes(n_samples_to_read)
+wav_file.setpos(start_sample)
+wav_data = wav_file.readframes(n_samples_to_read)
 
 # Make 3 objects for printing
-pretty_hex_list = list(pretty_hex_string(symbols.hex()))
-int_list = list(bytes2int_list(symbols))
+pretty_hex_list = list(pretty_hex_string(wav_data.hex()))
+int_list = list(bytes2int_list(wav_data))
 dot_list = list(ints2dots(int_list))
 
 # Do the printing
@@ -33,13 +34,18 @@ print("Params:\n", wav_file.getparams())
 print()
 print("File duration (s) =", wav_file.getnframes() / sample_rate)
 print("Frames / FSK symbol =", samples_per_symbol)
-print("Bytes in %i FSK symbols =" % n_symbols_to_read, len(symbols))
+print("Bytes in %i FSK symbols =" % n_symbols_to_read, len(wav_data))
+print("Seconds read =", n_samples_to_read / sample_rate)
 print()
 print("First %i bytes (%i samples):" % (n_bytes_to_plot, n_frames_to_plot))
-print(symbols[:n_bytes_to_plot])
+print(wav_data[:n_bytes_to_plot])
 print()
-print(''.join(pretty_hex_list[:n_bytes_to_plot * char_per_byte]))
+print(''.join(pretty_hex_list[:n_bytes_to_plot * char_per_byte]))  # pretty hex list
 print()
-print(int_list[:n_bytes_to_plot // bytes_per_sample])
+print(int_list[:n_bytes_to_plot // bytes_per_sample])  # int list
 print()
-print('\n'.join(dot_list[:n_bytes_to_plot // bytes_per_sample]))
+print('\n'.join(dot_list[:n_bytes_to_plot // bytes_per_sample]))  # dot list
+
+f, t, Zxx = signal.stft(int_list, fs=sample_rate)
+print(f)
+print(t)
